fc75d3bb06e327bf70168f0d1919cc6c0e9a8645
Chimera <j.calderon3@gmail.com>
2021-03-09 20:42:03 -0500

    SearchForm script added to components directory

---
+++ my-app/src/components/SearchForm.js
@@ -0,0 +1,33 @@
+// See unit 19, activity 23
+
+import React from "react";
+
+function SearchForm(props) {
+  return (
+    <form className="search">
+      <div className="form-group">
+        <label htmlFor="employee">Employee Name:</label>
+        <input
+          value={props.search}
+          onChange={props.handleInputChange}
+          name="search"
+          list="employees"
+          type="text"
+          className="form-control"
+          placeholder="Type in a name to begin search"
+          id="search"
+        />
+        <datalist id="employees">
+          {props.employees.map(employee => (
+            <option value={employee} key={employee} />
+          ))}
+        </datalist>
+        <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
+          Search
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default SearchForm;
---
+++ my-app/src/components/Wrapper.js
@@ -0,0 +1,8 @@
+// See unit 19, activity 23
+import React from "react";
+
+function Wrapper(props) {
+  return <main className="wrapper">{props.children}</main>;
+}
+
+export default Wrapper;
